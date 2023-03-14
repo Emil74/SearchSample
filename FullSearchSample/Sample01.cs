ba0dfdd363a7058d12618515f622e9a16a593788
Emil74 <emil_nagiyev_98@mail.ru>
2023-03-14 14:28:34 +0300

    Lesson_7

---
+++ FullSearchSample/Sample01.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Engines;
+using FullSearchSample;
+using FullSearchSample.Services;
+using FullSearchSample.Services.Impl;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullSearchSample
+{
+    public class Sample01
+    {
+        static void Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services => {
+
+                    #region Configure EF DBContext Service (CardStorageService Database)
+
+                    services.AddDbContext<DocumentDbContext>(options =>
+                    {
+                        options.UseSqlServer(@"data source=DESKTOP-T63QT50\SQLEXPRESS;initial catalog=DocumentsDatabase;MultipleActiveResultSets=True;App=EntityFramework;Trusted_Connection=True;TrustServerCertificate=True;");
+                    });
+
+                    #endregion
+
+                    #region Configure Repositories
+
+                    services.AddTransient<IDocumentRepository, DocumentRepository>();
+
+                    #endregion
+
+
+                })
+                .Build();
+
+            // Сохраним документы в БД
+            host.Services.GetRequiredService<IDocumentRepository>().LoadDocuments();
+
+
+        }
+    }
+}
